29c39172eb1f1079ac4e1e16ba1474fced9ef2f5
Mirinth <mirinth@gmail.com>
2014-05-09 22:14:11 -0600

    Cleanup, bug fix
    -Removed duplicate files from /Views/Raindrop
    -Removed accidentally included /Models/Article.cs
    -Fixed /Controllers/HomeController to use DemoModel
    -Fixed /Views/Home/Index.rdl to match DemoModel

--- RaindropDemo/Controllers/HomeController.cs
+++ RaindropDemo/Controllers/HomeController.cs
@@ -8,35 +8,10 @@ namespace RaindropDemo
 {
     public class HomeController : Controller
     {
-        private ArticleDBContext db = new ArticleDBContext();
-
         public ActionResult Index()
         {
-            var query = from a in db.Articles
-                        orderby a.Date descending
-                        select a;
+            ViewData = DemoModel.GetData();
 
-            Article result = null;
-
-            foreach (var first in query.Take(1))
-            {
-                result = first;
-            }
-
-            if (result != null)
-            {
-                ViewData.Add("title", result.Title);
-                ViewData.Add("date", result.Date);
-                ViewData.Add("id", result.ID);
-                ViewData.Add("content", result.Content);
-            }
-            else
-            {
-                ViewData.Add("title", "No articles.");
-                ViewData.Add("date", DateTime.Now);
-                ViewData.Add("id", -1);
-                ViewData.Add("content", "No content.");
-            }
 
             return View();
         }
