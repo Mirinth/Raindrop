1938398afd257e952f514d840d737813e360cf84
Mirinth <mirinth@gmail.com>
2014-05-26 20:50:10 -0600

    Applied licensing

--- Raindrop/Raindrop/Tags/Base tags/BlockTag.cs
+++ Raindrop/Raindrop/Tags/Base tags/BlockTag.cs
@@ -1,10 +1,30 @@
 ﻿/*
- * BlockTag.cs
- * By Mirinth (mirinth@gmail.com)
+ * Copyright 2014
  * 
- * The BlockTag file contains the BlockTag class, which represents a
- * generic tag that contains children.
+ * This file is part of the Raindrop Templating Library.
+ * 
+ * The Raindrop Templating Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Raindrop Templating Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the Raindrop Templating Library. If not, see
+ * <http://www.gnu.org/licenses/>. 
  */
+
+/*
+* BlockTag.cs
+* By Mirinth (mirinth@gmail.com)
+* 
+* The BlockTag file contains the BlockTag class, which represents a
+* generic tag that contains children.
+*/
 
 using System.Collections.Generic;
 using System.IO;
