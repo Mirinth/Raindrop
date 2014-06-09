f14faba55c632947daf5e62659079e095eb49db7
Mirinth <mirinth@gmail.com>
2014-06-09 17:59:44 -0600

    Removed hard-coded tags
    Tags are no longer hard-coded in the TagFactory; it now identifies them
    via reflection. Tags can now be implemented outside of the Raindrop
    library and still found.

--- Raindrop/Backend/TagFactory.cs
+++ Raindrop/Backend/TagFactory.cs
@@ -18,13 +18,32 @@
  * <http://www.gnu.org/licenses/>. 
  */
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Raindrop.Backend.Parser;
 using Raindrop.Backend.Tags;
 
 namespace Raindrop.Backend
 {
-    class TagFactory
+    public class TagFactory
     {
+        private static Dictionary<string, ConstructorInfo> itags;
+
+        /// <summary>
+        /// The TagFactory constructor.
+        /// </summary>
+        static TagFactory()
+        {
+            itags = GetITags();
+        }
+
+        /// <summary>
+        /// Parses an ITag out of the given TagStream.
+        /// </summary>
+        /// <param name="ts">The TagStream to parse an ITag from.</param>
+        /// <returns>The ITag parsed from the TagStream.</returns>
         public static ITag Parse(TagStream ts)
         {
             if (ts.EOF)
@@ -34,39 +53,7 @@ namespace Raindrop.Backend
 
             TagData td = ts.GetTag();
 
-            if (Handles(td.ID, TextTag.ID))
-            {
-                return new TextTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, ArrayTag.ID))
-            {
-                return new ArrayTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, ArrayEndTag.ID))
-            {
-                return new ArrayEndTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, CondTag.ID))
-            {
-                return new CondTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, CondEndTag.ID))
-            {
-                return new CondEndTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, DataTag.ID))
-            {
-                return new DataTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, NCondTag.ID))
-            {
-                return new NCondTag(td.Param, ts);
-            }
-            else if (Handles(td.ID, NCondEndTag.ID))
-            {
-                return new NCondEndTag(td.Param, ts);
-            }
-            else
+            if (!itags.ContainsKey(td.ID))
             {
                 string msg = string.Format(
                     "The tag '{0}' is not supported.",
@@ -76,21 +63,57 @@ namespace Raindrop.Backend
                     ts.Name,
                     ts.Index);
             }
+
+            ConstructorInfo constructor = itags[td.ID];
+
+            // Invoke the constructor with the parameter and TagStream.
+            ITag tag = (ITag)constructor.Invoke(new object[] { td.Param, ts });
+
+            return tag;
         }
 
         /// <summary>
-        /// Tests whether the given testId is handled by the tag that
-        /// has the given tagId.
+        /// Generates a Dictionary of Types that implement the ITag interface,
+        /// are not abstract, and have a public static field named "ID" which
+        /// is a string. The  Dictionary is indexed by the value of the type's
+        /// "ID" property, and all loaded assemblies are searched for objects.
         /// </summary>
-        /// <param name="testId">The ID to be tested.</param>
-        /// <param name="tagId">The ID of the tag that may handle the testIDd.</param>
-        /// <returns>
-        /// Whether the given testId is handled by the tag that has
-        /// the given tagId.
-        /// </returns>
-        private static bool Handles(string testId, string tagId)
+        /// <returns>A Dictionary containing the identified types.</returns>
+        public static Dictionary<string, ConstructorInfo> GetITags()
         {
-            return (testId == tagId);
+            const BindingFlags flags = BindingFlags.Public | BindingFlags.Static;
+            Type tagInterface = typeof(ITag);
+            Dictionary<string, ConstructorInfo> types = new Dictionary<string, ConstructorInfo>();
+
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type t in asm.GetTypes())
+                {
+                    if (!t.GetInterfaces().Contains(tagInterface)) { continue; }
+                    if (t.IsAbstract) { continue; }
+                    if (t.GetField("ID", flags) == null) { continue; }
+
+                    object key = t.GetField("ID").GetValue(null);
+
+                    // ITags with non-string ID fields can't be used, so
+                    // just skip them.
+                    if (!(key is string)) { continue; }
+
+                    ConstructorInfo constructor = t.GetConstructor(
+                        new Type[] { typeof(string), typeof(TagStream) });
+
+                    // If it doesn't implement the (string,TagStream) constructor,
+                    // then it can't be used, so skip it.
+                    if (constructor == null) { continue; }
+
+                    // Finally out of things to filter!
+                    // Key is a string; else it would have been filtered by the
+                    // check for (key is string) above. Casting is safe.
+                    types.Add((string)key, constructor);
+                }
+            }
+
+            return types;
         }
     }
 }
