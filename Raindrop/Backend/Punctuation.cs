fee8202d641d4229cf2b924b935a0e7f10da00f0
Mirinth <mirinth@gmail.com>
2014-08-10 12:22:17 -0600

    Added Punctuation class
    - The Punctuation class contains all the punctuation characters in a
    central place, which is necessary because the lexer and parser both need
    to access them.

---
+++ Raindrop/Backend/Punctuation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Raindrop.Backend
+{
+    class Punctuation
+    {
+        public static readonly string LeftCap = "<:";
+        public static readonly string RightCap = ":>";
+        public static readonly char[] Trim = { ' ', '/' };
+        public static readonly char Divider = ' ';
+    }
+}
