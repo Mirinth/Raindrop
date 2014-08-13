ffd135f16a4b5f85ae2e5ecd615f8a620d3442c3
Mirinth <mirinth@gmail.com>
2014-08-13 14:11:58 -0600

    Readonly strings const now
    Punctuation's readonly strings are const now. The char arrays can't be
    made const too, unfortunately, because the language doesn't allow it.

--- Raindrop/Backend/Punctuation.cs
+++ Raindrop/Backend/Punctuation.cs
@@ -30,9 +30,10 @@ namespace Raindrop.Backend
 {
     static class Punctuation
     {
-        public static readonly string LeftCap = "<:";
-        public static readonly string RightCap = ":>";
-        public static readonly string Divider = " ";
+        public const string LeftCap = "<:";
+        public const string RightCap = ":>";
+        public const string Divider = " ";
+
         public static readonly char[] ParamTrim = { ' ', '\r', '\n', '\t', '/' };
         public static readonly char[] NameTrim = { ' ', '\r', '\n', '\t' };
 
